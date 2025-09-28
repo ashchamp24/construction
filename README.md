c5326747145980c45974e35602d7ca93e00c1031
ashchamp24 <ashilshah01@gmail.com>
2025-09-28 13:43:14 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple Foundry Construction Site
+
+This is a very simple React + Vite starter.
+
+## Run locally
+npm install
+npm run dev
+
+## Build for production
+npm run build
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>Foundry Construction</h1>
+      <p>Website coming soon.</p>
+    </div>
+  )
+}
